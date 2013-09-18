23918e1817362cec4bd4cce1153cffeddcc72683
Ivan Malison <imalison@yelp.com>
2013-09-17 19:11:20 -0700

    Make things less terrible.

--- stream_command.py
+++ stream_command.py
@@ -4,18 +4,47 @@ import sys
 import time
 from optparse import OptionParser
 
+
+class StreamCommandRunner(object):
+
+	def __init__(self, command, sleep_time):
+		self.command = command
+		self.sleep_time = sleep_time
+		self.last_time = None
+		self.read_last_time = True
+
+	def _accumulate_input(self):
+		time_to_stop_after = self.last_time + self.sleep_time
+		lines = []
+		new_time = time.time()
+
+		while new_time < time_to_stop_after:
+			if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+				lines.append(sys.stdin.readline())
+				self.read_last_time = True
+			elif self.read_last_time:
+				self.read_last_time = False
+			else:
+				time.sleep(1)
+			new_time = time.time()
+
+		self.last_time = new_time
+
+		return ''.join(lines)
+
+	def loop_indefinitely(self):
+		self.last_time = time.time()
+		while True:
+			Popen(
+				[self.command],
+				shell=True,
+				stdin=PIPE,
+			).communicate(self._accumulate_input())
+
+
 if __name__ == '__main__':
 	parser = OptionParser()
-	parser.add_option('-s', '--sleep-time', dest="sleep_time", action="store", type="float")
-	parser.add_option('-c', '--command', dest="command", action="store")
+	parser.add_option('-s', '--sleep-time', dest="sleep_time", action="store", type="float", default=1.0)
+	parser.add_option('-c', '--command', dest="command", action="store", default='cat')
 	options, _ = parser.parse_args()
-	while True:
-		time.sleep(options.sleep_time)
-		lines = []
-		while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
-			lines.append(sys.stdin.readline())
-		Popen(
-			[options.command],
-			shell=True,
-			stdin=PIPE,
-		).communicate(''.join(lines))
+	StreamCommandRunner(options.command, options.sleep_time).loop_indefinitely()
